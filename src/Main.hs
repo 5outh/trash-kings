87f389601de49ce0c1f3c23f78006cbf48d4d6f9
5outh <bkovach13@gmail.com>
2017-01-04 15:13:46 -0500

    Tiles

--- src/Main.hs
+++ src/Main.hs
@@ -1,41 +1,131 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE FlexibleContexts          #-}
 {-# LANGUAGE TypeFamilies              #-}
+{-# LANGUAGE LambdaCase                #-}
 
 module Main where
 
 import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 
-myCircle :: Diagram B
-myCircle = circle 1
-    # fc blue
-    # lw none
-    ||| circle 1 #  fc red # lw veryThick # lc purple
+data Corner = TL | TR | BL | BR
+    deriving (Show, Eq, Enum)
 
-example :: Diagram B
-example = square 1 # fc aqua `atop` circle 1
+data CDir = T | L | R | B
+    deriving (Show, Eq, Enum)
 
-tile :: Diagram B
-tile = rect (1/3) 1 `atop` square 1
-    ||| (square 1 `atop` rect 1 (1/3))
+directions :: [CDir]
+directions = enumFromTo T B
+
+corners :: [Corner]
+corners = enumFromTo TL BR
+
+opCorner :: Corner -> Corner
+opCorner = \case
+    TL -> BR
+    BR -> TL
+    TR -> BL
+    BL -> TR
 
 blankTile :: Diagram B
 blankTile = square 1
 
-donut :: Diagram B
-donut = circle (1/2) `atop` circle 1
-
-curvedRoad :: Diagram B
-curvedRoad = translate (r2 (1/2, -1/2)) $ annularWedge (2/3) (1/3) d a # showOrigin
+curvedRoad :: Corner -> Diagram B
+curvedRoad corner = trans corner $ annularWedge (2/3) (1/3) (d corner) a
     where
-        d :: Direction V2 Double
-        d = rotateBy (1/4) xDir
+        d :: Corner -> Direction V2 Double
+        d c = flip rotateBy xDir $ case c of 
+           BL -> 0
+           BR -> 1/4 
+           TR -> 2/4
+           TL -> 3/4
+
+        trans c = translate . r2 $ case c of 
+            BL -> (-1/2, -1/2)
+            BR -> (1/2, -1/2)
+            TR -> (1/2, 1/2)
+            TL -> (-1/2, 1/2)
 
         a :: Angle Double
         a = (tau / 4) @@ rad
 
+nub :: CDir -> Diagram B
+nub c = trans c $ wedge (1/6) (d c) a
+    where
+        d = flip rotateBy xDir . \case
+                B -> 0   
+                R -> 1/4
+                T -> 1/2
+                L -> 3/4
+        trans = translate . r2 . \case
+                    B -> (0, -1/2) 
+                    R -> (1/2, 0) 
+                    T -> (0, 1/2)
+                    L -> (-1/2, 0) 
+
+        a = (tau / 2) @@ rad
+
+nubTile :: CDir -> Diagram B
+nubTile c = nub c `atop` blankTile
+
+curvedRoadTile :: Corner -> Diagram B
+curvedRoadTile c = curvedRoad c `atop` blankTile
+
+doubleCurvedRoadTile :: Corner -> Diagram B
+doubleCurvedRoadTile c = curvedRoad c <> curvedRoad (opCorner c) <> blankTile
+
+curvedRoadTiles :: Diagram B
+curvedRoadTiles = hcat $ map curvedRoadTile (enumFromTo TL BR)
+
+doubleCurvedRoadTiles :: Diagram B
+doubleCurvedRoadTiles = hcat $ map doubleCurvedRoadTile [TL, TR]
+
+straightRoad = \case
+    T -> rect (1/3) 1 
+    B -> rect (1/3) 1
+    R -> rect 1 (1/3)
+    L -> rect 1 (1/3)
+
+straightRoadTile :: CDir -> Diagram B
+straightRoadTile = atop blankTile . straightRoad
+
+straightRoadTiles :: Diagram B
+straightRoadTiles = hcat $ map straightRoadTile [T,L]
+
+nubTiles = hcat $ map nubTile [T,L,B,R]
+
+tiles :: Diagram B
+tiles = vcat
+    [ curvedRoadTiles
+    , doubleCurvedRoadTiles
+    , straightRoadTiles
+    , nubTiles
+    ]
+
+straightWithNubsTile :: CDir -> Diagram B
+straightWithNubsTile c = blankTile <> straightRoad c <> case c of
+    T -> nub L <> nub R
+    R -> nub T <> nub B
+    B -> nub L <> nub R
+    L -> nub T <> nub B
+
+curveWithNubsTile :: Corner -> Diagram B
+curveWithNubsTile c = blankTile <> curvedRoad c <> case c of
+    TR -> nub B <> nub L
+    TL -> nub B <> nub R
+    BL -> nub T <> nub R
+    BR -> nub T <> nub L
+
+-- todo get all tiles as data
+fullTiles = vcat
+    [ mconcat (map nub directions) <> blankTile
+    , doubleCurvedRoadTiles
+    , hcat (map straightWithNubsTile [T,R]) 
+    , hcat (map curveWithNubsTile corners)
+    ]
+
 main :: IO ()
 main = do
     putStrLn "Generating new circle"
-    mainWith (curvedRoad `atop` blankTile # showOrigin) 
+    mainWith fullTiles 
+
