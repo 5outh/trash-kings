beb72dc64debdc560f5b9a032f3f3cc57f661eed
5outh <bkovach13@gmail.com>
2017-01-04 15:58:32 -0500

    Color

--- src/Main.hs
+++ src/Main.hs
@@ -27,11 +27,26 @@ opCorner = \case
     TR -> BL
     BL -> TR
 
+onTile = (<> blankTile)
+
 blankTile :: Diagram B
-blankTile = square 1
+blankTile = square 1 # fc lightgreen 
+
+-- Align a diagram to the top, right, left or bottom
+alignTile :: CDir -> Diagram B -> Diagram B
+alignTile dir diagram = rotate' diagram
+    where
+        rotate' =
+            rotateBy
+                $ case dir of
+                      T -> 0
+                      R -> 1/4
+                      B -> 1/2
+                      L -> 3/4
 
 curvedRoad :: Corner -> Diagram B
 curvedRoad corner = trans corner $ annularWedge (2/3) (1/3) (d corner) a
+    # fc lightblue
     where
         d :: Corner -> Direction V2 Double
         d c = flip rotateBy xDir $ case c of 
@@ -50,7 +65,7 @@ curvedRoad corner = trans corner $ annularWedge (2/3) (1/3) (d corner) a
         a = (tau / 4) @@ rad
 
 nub :: CDir -> Diagram B
-nub c = trans c $ wedge (1/6) (d c) a
+nub c = trans c $ wedge (1/6) (d c) a # fc yellow
     where
         d = flip rotateBy xDir . \case
                 B -> 0   
@@ -66,13 +81,13 @@ nub c = trans c $ wedge (1/6) (d c) a
         a = (tau / 2) @@ rad
 
 nubTile :: CDir -> Diagram B
-nubTile c = nub c `atop` blankTile
+nubTile c = onTile (nub c)
 
 curvedRoadTile :: Corner -> Diagram B
-curvedRoadTile c = curvedRoad c `atop` blankTile
+curvedRoadTile = onTile . curvedRoad
 
 doubleCurvedRoadTile :: Corner -> Diagram B
-doubleCurvedRoadTile c = curvedRoad c <> curvedRoad (opCorner c) <> blankTile
+doubleCurvedRoadTile c = onTile $ curvedRoad c <> curvedRoad (opCorner c)
 
 curvedRoadTiles :: Diagram B
 curvedRoadTiles = hcat $ map curvedRoadTile (enumFromTo TL BR)
@@ -80,14 +95,15 @@ curvedRoadTiles = hcat $ map curvedRoadTile (enumFromTo TL BR)
 doubleCurvedRoadTiles :: Diagram B
 doubleCurvedRoadTiles = hcat $ map doubleCurvedRoadTile [TL, TR]
 
-straightRoad = \case
+straightRoad :: CDir -> Diagram B
+straightRoad = fc pink . \case
     T -> rect (1/3) 1 
     B -> rect (1/3) 1
     R -> rect 1 (1/3)
     L -> rect 1 (1/3)
 
 straightRoadTile :: CDir -> Diagram B
-straightRoadTile = atop blankTile . straightRoad
+straightRoadTile = onTile . straightRoad
 
 straightRoadTiles :: Diagram B
 straightRoadTiles = hcat $ map straightRoadTile [T,L]
@@ -103,26 +119,28 @@ tiles = vcat
     ]
 
 straightWithNubsTile :: CDir -> Diagram B
-straightWithNubsTile c = blankTile <> straightRoad c <> case c of
+straightWithNubsTile c = onTile $ straightRoad c <> case c of
     T -> nub L <> nub R
     R -> nub T <> nub B
     B -> nub L <> nub R
     L -> nub T <> nub B
 
 curveWithNubsTile :: Corner -> Diagram B
-curveWithNubsTile c = blankTile <> curvedRoad c <> case c of
+curveWithNubsTile c = onTile $ curvedRoad c <> case c of
     TR -> nub B <> nub L
     TL -> nub B <> nub R
     BL -> nub T <> nub R
     BR -> nub T <> nub L
 
 undies :: Diagram B
-undies = translate (r2 (0, 1/6)) $ roundedRect' 1 (2/3) opts 
+undies = translate (r2 (0, 1/6)) $ roundedRect' 1 (2/3) opts # fc palegoldenrod
     where 
         opts = with & radiusTL .~ -1/3
                     & radiusTR .~ -1/3
 
-undiesWithNub rot = rotateBy rot $ undies <> nub B <> blankTile
+undiesWithNub rot = rotateBy rot $ undies <> nub B
+
+undiesWithNubTile = onTile . undiesWithNub
 
 -- todo get all tiles as data
 -- todo colors
@@ -131,7 +149,7 @@ fullTiles = vcat
     , doubleCurvedRoadTiles
     , hcat (map straightWithNubsTile [T,R]) 
     , hcat (map curveWithNubsTile corners)
-    , hcat (map undiesWithNub [0, 1/4, 1/2, 3/4])
+    , hcat (map undiesWithNubTile [0, 1/4, 1/2, 3/4])
     ]
 
 main :: IO ()
