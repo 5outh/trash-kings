75d30ede70ebfe853db625749d3960a005797c8b
5outh <bkovach13@gmail.com>
2017-01-08 13:22:15 -0500

    Generate arrowed thing

--- src/TrashKings/Examples.hs
+++ src/TrashKings/Examples.hs
@@ -1,19 +1,33 @@
+{-# LANGUAGE FlexibleContexts #-}
 module TrashKings.Examples where
 
-import Data.Monoid
 import Diagrams.Backend.SVG
 import Diagrams.TwoD.Arrow
 import Diagrams.TwoD.Size
+import Diagrams.Prelude
 import TrashKings.Layout
 import TrashKings.Types
+import Control.Monad.Random
+import TrashKings.Generation
 
 -- Generate a named example
 genExample :: FilePath -> Diagram' -> IO ()
 genExample filepath =
-    renderSVG ("examples/" <> filepath) (mkWidth 1000)
+    renderSVG
+        ("examples/" <> filepath <> ".svg")
+        (mkWidth 1000)
 
+example sDot eDot sPt ePt = 
+    sDot # named "1" # moveTo sPt
+    <> eDot # named "2" # moveTo ePt
+
+doIt :: MonadRandom m => m Diagram'
 doIt = do
-    let p1 = (0, 0)
-        p2 = (3, 0)
-    undefined
+    let pA = p2 (0, 0)
+        pB = p2 (3, 0)
+    (t1, t2) <- (,) <$> genTile <*> genTile
+    pure $ connectOutside "1" "2" $ example t1 t2 pA pB
 
+go = do
+    tile <- evalRandIO doIt
+    genExample "test" tile
